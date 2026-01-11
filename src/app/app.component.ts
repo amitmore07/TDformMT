1252e789bd27d009c3dcc78da79842c0c1fd73f5
amitmore07 <amitmore50921@gmail.com>
2026-01-11 15:26:29 +0530

    TDformMT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TDformMT';
+  @ViewChild('singUpForm') singUpForm! : NgForm
+
+  userDetails={
+    userName:'Amit More',
+    Email: 'amitmore50921@gmail.com',
+    secretQuestion: 'FavSport',
+    answer:'Cricket',
+    isSubscribed:true,
+    modeofContact:'Phone'
+  }
+
+  mocArr =[
+    {
+
+      contactMode: 'Email',
+      id: 123
+    },
+   {
+
+      contactMode: 'Phone',
+      id: 124
+    },
+  ]
+
+
+  ngOnInit(): void{
+
+  }
+
+  onSubmit(){
+
+  }
+
+  patchdata(){
+    this.singUpForm.form.patchValue(this.userDetails)
+  }
+  
 }
